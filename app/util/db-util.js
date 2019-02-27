9796e2693e03f5eb894f8511f264ca249c6406ce
Riccardo Dalla Via <rikydallavia1997@gmail.com>
2019-02-27 22:30:42 +0100

    Working on network states update

--- app/util/db-util.js
+++ app/util/db-util.js
@@ -1,32 +1,28 @@
 const request = require('request');
-const {Network} = require('gnb-network/node');
 
 let exports = module.exports = {};
 
 /**
- * TODO complete documentation
+ * Insert data in a specific database
  * @param httpUrl {string} url of the database
  * @param queryParams {string} query parameters
- * @param data
+ * @param data {string} query data
  */
-function writeOnDb(httpUrl,
-                   queryParams,
-                   data){
+function writeOnDb(httpUrl, queryParams, data){
     request
         .post(`${httpUrl}${queryParams}`, {form:data})
         .on('response', (res) => {
             return res.statusCode;
         });
-
 }
 
 /**
+ * TODO this function is not returning anything back
  * Query data from specific database
  * @param httpUrl {string} url of the database
  * @param queryParams {string} query parameters
  */
-function readFromDb(httpUrl,
-                    queryParams) {
+function readFromDb(httpUrl, queryParams) {
     request
         .get(`${httpUrl}${queryParams}`)
         .on('error', function(err) {
@@ -39,30 +35,30 @@ function readFromDb(httpUrl,
 }
 
 /**
- * Writes on db the updated values of the network's state.
- * @param network {Network} reference to network obj
+ * Writes on db the updated values of the network's node states.
+ * @param network {Network} reference to network object
  * @param updatedValues {JSON} JSON containing for each node of a network, the updated states' values.
  * @returns something
  * */
-function updateNetwork(network, updatedValues) {
+function writeNetworkStates(network, updatedValues) {
     //assemble httpUrl
-    let httpUrl = newtwork.DBWriteUrl;
+    let httpUrl = network.DBWriteUrl;
 
     //compose queryParams
-    let queryParams = `/write/db=${newtwork.DBWriteName}&precision=s`;
+    let queryParams = `/write/db=${network.DBWriteName}&precision=s`;
 
     //scan updatedValues and compose dataToSend
     let dataToSend = "";
-    for (let node in updatedValues) {
-        for (let node_state in node.states) {
-            dataToSend.concat(`${network.name},${node.id}_${node_state.name}=${node_state.value}\n`);
-        }
-    }
-    console.log(dataToSend);
+    updatedValues.nodes.forEach(node => {
+       node.states.forEach(state => {
+           dataToSend.concat(`${network.name},${node.id}_${state.name}=${state.value}\n`);
+       })
+    });
+    console.log(dataToSend); //TODO remove this
     //call to writeOnDb
     writeOnDb(httpUrl, queryParams, dataToSend);
 }
 
 exports.writeOnDb = writeOnDb;
 exports.readFromDb = readFromDb;
-exports.updateNetwork = updateNetwork;
+exports.writeNetworkStates = writeNetworkStates;
--- app/util/network-util.js
+++ app/util/network-util.js
@@ -1,5 +1,8 @@
+const { writeNetworkStates, readFromDb } = require('./db-util');
+
+let exports = module.exports = {};
+
 /**
- * TODO
  * Calculates and updates current node values and state
  * both in jsbayes and in the database
  * @param network {Network} the network to update
@@ -8,12 +11,32 @@ function updateNetwork(network) {
     //update node states
     network.nodes.forEach(node => {
         if(node.hasSensor()) {
-            let value = 0; //todo fix this
-            network.graph.node(node.id).observe(node.updateState(value));
+            let value = getSensorValue(node);
+            let state = node.getState(value);
+
+            network.graph.observe(node.id, state);
         }
     });
-
     network.graph.sample(20000);
+
+    //query node states
+    let states = {};
+    network.nodes.forEach(node => {
+        console.log(network.graph.node(node.id).probs()); //TODO
+        //writeNetworkStates(network, states);
+    });
+}
+
+/**
+ * TODO
+ * Get the current value of the sensor
+ * connected by the node
+ * @param node {Node} the node connected to the sensor
+ * @returns {number} the value of the sensor
+ */
+function getSensorValue(node) {
+    //check node.sensor to see the database values
+    return 0;
 }
 
 exports.updateNetwork = updateNetwork;
