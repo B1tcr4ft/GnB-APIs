e9e50f4054165fb2b078d0dde09ceb6332ec7061
Riccardo Dalla Via <rikydallavia1997@gmail.com>
2019-02-28 00:09:23 +0100

    Better implementation of /update/api

--- app/routes/update.js
+++ app/routes/update.js
@@ -1,47 +1,58 @@
-const { exec } = require('child_process');
+const { exec, spawn } = require('child_process');
 const MY_SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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';
 const slack = require('slack-notify')(MY_SLACK_WEBHOOK_URL);
 
 module.exports = function(app) {
 
     app.post('/update/api', (req, res) => {
-        exec('cd /home/gnb-backend && git pull');
+        let oldTime = (new Date()).getTime();
+        sendSlackMessage('*API services updating...*', 'API services updating...', '#ffb347');
 
-        console.log("Reloading API");
+        exec('cd /home/gnb-backend && git pull', (error) => {
+            if(error) {
+                sendSlackMessage('*API services error:* ' + error, 'API services error: ' + error, '#ff6961');
+            } else {
+                exec('cd /home/gnb-backend && npm install', (error) => {
+                    if (error) {
+                        sendSlackMessage('*API services error:* ' + error, 'API services error: ' + error, '#ff6961');
+                    } else {
+                        let amountTime = ((new Date()).getTime() - oldTime)/1000;
+                        sendSlackMessage('*API services updated!* (' + amountTime + 's)', 'API services updated! (' + amountTime + 's)', '#77dd77');
 
-        setTimeout(function () {
-            process.on("exit", function () {
-                require("child_process").spawn(process.argv.shift(), process.argv, {
-                    cwd: process.cwd(),
-                    detached : true,
-                    stdio: "inherit"
+                        process.on("exit", function () {
+                            spawn(process.argv.shift(), process.argv, {
+                                cwd: process.cwd(),
+                                detached : true,
+                                stdio: "inherit"
+                            });
+                        });
+                        process.exit();
+                    }
                 });
-            });
-            process.exit();
-        }, 5000);
+            }
+        });
 
-        sendSlackMessage('*API services restarting!*', 'API services restarting!', '#77dd77');
         res.send('updated');
     });
 
     app.post('/update/grafana', (req, res) => {
         let oldTime = (new Date()).getTime();
-        sendSlackMessage('*Grafana updating...*', 'Grafana updating...', '#ffb347');
+        sendSlackMessage('*Grafana plugin updating...*', 'Grafana plugin updating...', '#ffb347');
 
         exec('cd /var/lib/grafana/plugins/gnb && git pull', (error) => {
             if(error) {
-                sendSlackMessage('*Grafana error:* ' + error, 'Grafana error: ' + error, '#ff6961');
+                sendSlackMessage('*Grafana plugin error:* ' + error, 'Grafana plugin error: ' + error, '#ff6961');
             } else {
                 exec('cd /var/lib/grafana/plugins/gnb && npm install', (error) => {
                     if(error) {
-                        sendSlackMessage('*Grafana error:* ' + error, 'Grafana error: ' + error, '#ff6961');
+                        sendSlackMessage('*Grafana plugin error:* ' + error, 'Grafana plugin error: ' + error, '#ff6961');
                     } else {
                         exec('sudo service grafana-server restart', (error) => {
                             if(error) {
-                                sendSlackMessage('*Grafana error:* ' + error, 'Grafana error: ' + error, '#ff6961');
+                                sendSlackMessage('*Grafana plugin error:* ' + error, 'Grafana plugin error: ' + error, '#ff6961');
                             } else {
                                 let amountTime = ((new Date()).getTime() - oldTime)/1000;
-                                sendSlackMessage('*Grafana updated!* (' + amountTime + 's)', 'Grafana updated! (' + amountTime + 's)', '#77dd77');
+                                sendSlackMessage('*Grafana plugin updated!* (' + amountTime + 's)', 'Grafana plugin updated! (' + amountTime + 's)', '#77dd77');
                             }
                         });
                     }
