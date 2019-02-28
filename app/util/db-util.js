f6ae5b3b6594116ac1b9c5bc63c3cf6f451b8b6c
Riccardo Dalla Via <rikydallavia1997@gmail.com>
2019-02-28 13:48:24 +0100

    Moved writeNetworkStates function

--- app/util/db-util.js
+++ app/util/db-util.js
@@ -36,31 +36,6 @@ function readFromDb(httpUrl, queryParams) {
         });
 }
 
-/**TODO move to network-util.js
- * Writes on db the updated values of the network's node states.
- * @param network {Network} reference to network object
- * @param updatedValues {JSON} JSON containing for each node of a network, the updated states' values.
- * @returns something
- * */
-function writeNetworkStates(network, updatedValues) {
-    //assemble httpUrl
-    let httpUrl = network.DBWriteUrl;
-
-    //compose queryParams
-    let queryParams = `/write/db=${network.DBWriteName}&precision=s`;
-
-    //scan updatedValues and compose dataToSend
-    let dataToSend = "";
-    updatedValues.nodes.forEach(node => {
-       node.states.forEach(state => {
-           dataToSend.concat(`${network.name},${node.id}_${state.name}=${state.value}\n`);
-       })
-    });
-    console.log(dataToSend); //TODO remove this
-    //call to writeOnDb
-    writeOnDb(httpUrl, queryParams, dataToSend);
-}
-
 exports.writeOnDb = writeOnDb;
 exports.readFromDb = readFromDb;
 exports.writeNetworkStates = writeNetworkStates;
--- app/util/network-util.js
+++ app/util/network-util.js
@@ -1,4 +1,4 @@
-const { writeNetworkStates, readFromDb } = require('./db-util');
+const { writeOnDb, readFromDb } = require('./db-util');
 
 var exports = module.exports = {};
 
@@ -20,7 +20,6 @@ function updateNetwork(network) {
     network.graph.sample(20000);
 
     //query node states
-    let states = {};
     network.nodes.forEach(node => {
         console.log(network.graph.node(node.id).probs()); //TODO
         //writeNetworkStates(network, states);
@@ -38,6 +37,31 @@ function getSensorValue(sensor) {
     let queryParams = `/query?db=${sensor.DBSensorName}&q=SELECT ${sensor.DBSensorColumn} from ${sensor.DBSensorTable} ORDER BY time DESC LIMIT 1`;
     let values = readFromDb(httpUrl, queryParams);
     return values[0][1];
+}
+
+/**
+ * Writes on db the updated values of the network's node states.
+ * @param network {Network} reference to network object
+ * @param updatedValues {JSON} JSON containing for each node of a network, the updated states' values.
+ * @returns something
+ * */
+function writeNetworkStates(network, updatedValues) {
+    //assemble httpUrl
+    let httpUrl = network.DBWriteUrl;
+
+    //compose queryParams
+    let queryParams = `/write/db=${network.DBWriteName}&precision=s`;
+
+    //scan updatedValues and compose dataToSend
+    let dataToSend = "";
+    updatedValues.nodes.forEach(node => {
+        node.states.forEach(state => {
+            dataToSend.concat(`${network.name},${node.id}_${state.name}=${state.value}\n`);
+        })
+    });
+    console.log(dataToSend); //TODO remove this
+    //call to writeOnDb
+    writeOnDb(httpUrl, queryParams, dataToSend);
 }
 
 exports.updateNetwork = updateNetwork;
