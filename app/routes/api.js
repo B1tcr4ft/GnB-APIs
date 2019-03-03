d678fdb79dd6bd6f3d3189307b2fe5b24e947abb
Riccardo Dalla Via <rikydallavia1997@gmail.com>
2019-03-03 15:57:40 +0100

    Testing out message errors

--- app/routes/api.js
+++ app/routes/api.js
@@ -62,7 +62,7 @@ module.exports = app => {
 
     app.get('/api/start/:id', (req, res) => {
         if (clockList[req.params.id]) {
-            res.error('Process already started for this network.');
+            throw 'Process already started for this network.';
         } else {
             let filePath = `./public/network_${req.params.id}.json`;
             fs.readFile(filePath, (error, data) => {
