ed8ac1812a36cfae112e0697f13b5aa3f3df5543
Riccardo Dalla Via <rikydallavia1997@gmail.com>
2019-03-04 17:02:51 +0100

    Fixes

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, Authorization, Content-Length, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
